bed692d18df3f3f136db792f72e8306104d7c8e2
Josue Fernandes <josue7mf@hotmail.com>
2023-01-13 20:08:47 -0300

    req get

--- JavaScript Topicos Avancados/req get/index.js
+++ JavaScript Topicos Avancados/req get/index.js
@@ -1,27 +1,25 @@
-let main = document.querySelector('#countries')
-
 function createCountryCard(country) {
-  const card = document.createElement('div');
-  card.classList.add('country');
-  
-  const countryName = country.name.common;
-  const conutryFlagSvg = country.flags.svg;
+  const card = document.createElement('div')
+  card.classList.add('country')
 
-  const name = document.createElement('h2');
-  name.textContent = countryName;
+  const countryName = country.name.common
+  const name = document.createElement('h2')
+  name.textContent = countryName
 
-  const flag = document.createElement('img');
-  flag.src = conutryFlagSvg;
+  const flag = document.createElement('img')
+  flag.src = country.flags.svg
+  flag.alt = countryName
 
-  card.append(...[name,flag])
-  main.append(card);
+  card.append(name, flag)
+  document.querySelector('#countries').append(card)
 }
 
 async function getCountries() {
-  const response = await fetch("https://restcountries.com/v3.1/all")
+  const response = await fetch('https://restcountries.com/v3.1/all')
   const countries = await response.json()
-  console.log(countries[0].name.common);
-  countries.forEach(country => createCountryCard(country))
+
+  console.log(countries)
+  countries.forEach(createCountryCard)
 }
 
-getCountries();
+getCountries()
