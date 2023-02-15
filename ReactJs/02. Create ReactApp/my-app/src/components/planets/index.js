4fbed5a0ac39eb388c43953e498bbbe54ab00e27
Josue Fernandes <josue7mf@hotmail.com>
2023-02-15 15:24:55 -0300

    exercício com 2 apis e  uso dos states

--- ReactJs/02. Create ReactApp/my-app/src/components/planets/index.js
+++ ReactJs/02. Create ReactApp/my-app/src/components/planets/index.js
@@ -1,26 +1,27 @@
 import React, { Fragment } from 'react'
 import Planet from './planet'
+
+async function getPlanets() {
+  let response = await fetch('http://localhost:3000/api/planets.json')
+  let data = await response.json();
+  return data.planets;
+} 
+
+
 class Planets extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      planets: [
-        {
-          name: 'Mercúrio',
-          description: 'Mercúrio bla bla bla........',
-          img_url:
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg',
-          link: 'https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)',
-        },
-        {
-          name: 'Plutão',
-          description: 'Plutão bla bla bla........',
-          img_url:
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg',
-          link: 'https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)',
-        },
-      ],
+      planets: [],
     }
+  }
+
+  componentDidMount() {
+    getPlanets().then(newPlanets => {
+      this.setState(state => ({
+        planets: newPlanets
+      }))
+    })
   }
 
   removeLast = () => {
@@ -46,6 +47,7 @@ class Planets extends React.Component {
         {this.state.planets.map((planet, index) => (
           <Planet
             key={index}
+            id={planet.id}
             name={planet.name}
             description={planet.description}
             img_url={planet.img_url}
--- ReactJs/02. Create ReactApp/my-app/src/components/planets/planet/index.js
+++ ReactJs/02. Create ReactApp/my-app/src/components/planets/planet/index.js
@@ -2,24 +2,56 @@ import React from 'react'
 import GrayImg from '../../shared/gray_img'
 import DescriptionWithLink from '../../shared/index'
 
-const Planet = (props) => {
-
-
-  let title;
-  if(props.title_with_underline) 
-    title = <h4><u>{props.name}</u></h4>
-  else 
-    title = <h4>{props.name}</h4>
-  
-  return (
-    <div>
-      {title}
-      <hr />
-      <DescriptionWithLink description = {props.description} text = {props.text}link={props.link} />
-      <GrayImg img_url={props.img_url} gray = {props.gray}/>
-      <hr/>
-    </div>
-  )
+async function getSatelites(id) {
+  let response = await fetch(`http://localhost:3000/api/${id}.json`)
+  let data = await response.json()
+  return data // retorna um objeto que tem satelites
 }
+class Planet extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      satelites: [],
+    }
+  }
 
+  componentDidMount() {
+    getSatelites(this.props.id).then(data => {
+      this.setState(state => ({
+        satelites: data['satelite']
+      }))
+    })
+  }
+  render() {
+    let title
+    if (this.props.title_with_underline)
+      title = (
+        <h4>
+          <u>{this.props.name}</u>
+        </h4>
+      )
+    else title = <h4>{this.props.name}</h4>
+
+    return (
+      <div>
+        {title}
+        <hr />
+        <DescriptionWithLink
+          description={this.props.description}
+          text={this.props.text}
+          link={this.props.link}
+        />
+        <GrayImg img_url={this.props.img_url} gray={this.props.gray} />
+        <h4>Satelites</h4>
+        <ul>
+          {
+            this.state.satelites.map((satelite,index) => <li key={index}>{satelite.name}</li>
+            )
+          }
+        </ul>
+        <hr />
+      </div>
+    )
+  }
+}
 export default Planet
