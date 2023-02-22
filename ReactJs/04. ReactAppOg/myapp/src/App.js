7a56b35c1198500c614e3f6d6fdcdc238fba381c
Josue Fernandes <josue7mf@hotmail.com>
2023-02-22 10:06:56 -0300

    style inline react

--- ReactJs/04. ReactAppOg/myapp/src/App.js
+++ ReactJs/04. ReactAppOg/myapp/src/App.js
@@ -1,43 +1,42 @@
-// Mostre os dados da aplicação, como aprensetado no vídeo
-// Não utilize CSS externo, use o style para mudar as cores
-// Se a situação estiver ativa pinte de verde, inativa vermelho
-// Se o gasto for maior que 10000 mostre uma mensagem
-const luana = {
-  cliente: 'Luana',
-  idade: 27,
-  compras: [
-    { nome: 'Notebook', preco: 'R$ 2500' },
-    { nome: 'Geladeira', preco: 'R$ 3000' },
-    { nome: 'Smartphone', preco: 'R$ 1500' },
-  ],
-  ativa: true,
-}
-
-const mario = {
-  cliente: 'Mario',
-  idade: 31,
-  compras: [
-    { nome: 'Notebook', preco: 'R$ 2500' },
-    { nome: 'Geladeira', preco: 'R$ 3000' },
-    { nome: 'Smartphone', preco: 'R$ 1500' },
-    { nome: 'Guitarra', preco: 'R$ 3500' },
-  ],
-  ativa: false,
-}
+// Organize os produtos como mostrado no vídeo
+// Mostre apenas produtos que forem mais caros que R$ 1500
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786'],
+  },
+];
 
 const App = () => {
-  const dados = mario
-  const gastoTotal = dados.compras.reduce((a,compra) => a + Number(compra.preco.replace("R$ ", "")),0)
-  return (
-    <div>
-      <p>Nome: {dados.cliente}</p>
-      <p>idade: {dados.idade}</p>
-      <p >Situação: <span style = {dados.ativa ? {color:"green"} : {color:"red"}}>{dados.ativa ? "Ativa" : "Inativa"}</span></p>
-      <p>Nome: {dados.cliente}</p>
-      <p>Total gasto: R${gastoTotal}</p>
-      {gastoTotal > 10000 && <p>"Você está gastando muito"</p>}
-    </div>
-  )
-}
+  const dados = produtos.filter(item => Number(item.preco.replace("R$ ","") > 1500));
+  return <section>
+    {dados.map(({id, nome,preco,cores}) => (
+      <div key={id}>
+        <h1>{nome}</h1>
+        <p>Preço: {preco}</p>
+        <ul>
+          
+          {cores.map(cor => (
+            <li key={cor} style={{color:"black", backgroundColor:cor}}>{cor}</li>
+          ))}
+        </ul>
+      </div>
+    ))}
+  </section>;
+};
 
 export default App
