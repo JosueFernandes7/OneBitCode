4a7993080012f0da9fc39ed0b2e193988c2099e5
Josue Fernandes <josue7mf@hotmail.com>
2023-02-27 10:50:40 -0300

    customHook useFetch

--- ReactJs/04. ReactAppOg/myapp/src/App.js
+++ ReactJs/04. ReactAppOg/myapp/src/App.js
@@ -1,12 +1,17 @@
-import useLocalStorage from "./hooks/useLocalStorage"
 
-function App() {
-  window.addEventListener('click',handleClick);
-  function handleClick(e) {
-    setProduto(e.target.innerHTML);
-  }
-  const [produto, setProduto] = useLocalStorage('produto','');
-  return <button >teste</button>
-}
+import useFetch from "./hooks/useFetch";
+import React from "react"
+const App = () => {
+  const { data, loading, error, request } = useFetch();
+
+  React.useEffect(() => {
+    request('https://ranekapi.origamid.dev/json/api/produto/noteboowk');
+  }, [request]);
+
+  if (error) return <p>{error}</p>;
+  if (loading) return <p>Carregando...</p>;
+  if (data) return <div>{data.nome}</div>;
+  else return null;
+};
 
 export default App
