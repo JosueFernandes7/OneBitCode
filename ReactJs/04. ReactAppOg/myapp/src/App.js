2ec205fdf03209ffd895be54a93afca43efeb24f
Josue Fernandes <josue7mf@hotmail.com>
2023-02-28 15:27:19 -0300

    input 2

--- ReactJs/04. ReactAppOg/myapp/src/App.js
+++ ReactJs/04. ReactAppOg/myapp/src/App.js
@@ -1,17 +1,98 @@
+import React from 'react'
 
-import useFetch from "./hooks/useFetch";
-import React from "react"
 const App = () => {
-  const { data, loading, error, request } = useFetch();
+  const formFields = [
+    {
+      id: 'nome',
+      label: 'Nome',
+      type: 'text',
+    },
+    {
+      id: 'email',
+      label: 'Email',
+      type: 'email',
+    },
+    {
+      id: 'senha',
+      label: 'Senha',
+      type: 'password',
+    },
+    {
+      id: 'cep',
+      label: 'Cep',
+      type: 'text',
+    },
+    {
+      id: 'rua',
+      label: 'Rua',
+      type: 'text',
+    },
+    {
+      id: 'numero',
+      label: 'Numero',
+      type: 'text',
+    },
+    {
+      id: 'bairro',
+      label: 'Bairro',
+      type: 'text',
+    },
+    {
+      id: 'cidade',
+      label: 'Cidade',
+      type: 'text',
+    },
+    {
+      id: 'estado',
+      label: 'Estado',
+      type: 'text',
+    },
+  ]
+  const [form, setForm] = React.useState(
+    formFields.reduce((acc, { id }) => {
+      return { ...acc, [id]: '' }
+    }, {})
+  )
 
-  React.useEffect(() => {
-    request('https://ranekapi.origamid.dev/json/api/produto/noteboowk');
-  }, [request]);
+  const [response, setResponse] = React.useState(null)
 
-  if (error) return <p>{error}</p>;
-  if (loading) return <p>Carregando...</p>;
-  if (data) return <div>{data.nome}</div>;
-  else return null;
-};
+  const useFetch = async (event) => {
+    event.preventDefault()
+    fetch('https://ranekapi.origamid.dev/json/api/usuario', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      // form é o objeto com os dados do formulário
+      body: JSON.stringify(form),
+    }).then((response) => {
+      setResponse(response)
+      console.log(response);
+    })
+  }
+  function handleChange({ target }) {
+    const { id, value } = target
+    console.log(id, value)
+    // setForm(...form, [id]: value);
+    setForm({ ...form, [id]: value })
+  }
+  return (
+    <form onSubmit={useFetch}>
+      {formFields.map(({ id, label, type }) => (
+        <div key={id}>
+          <label htmlFor={id}>{label}</label>
+          <input
+            type={type}
+            id={id}
+            value={form.id}
+            onChange={handleChange}
+          ></input>
+        </div>
+      ))}
+      <button>Enviar</button>
+      {response && response.ok && <p>Usuário Criado!!</p>}
+    </form>
+  )
+}
 
 export default App
