90765f42cc94b04fb6ed6a7550e6b9921381dc6f
Josue Fernandes <josue7mf@hotmail.com>
2023-03-13 14:54:30 -0300

    css import e css Modules

--- ReactJs/04. ReactAppOg/myapp/src/App.js
+++ ReactJs/04. ReactAppOg/myapp/src/App.js
@@ -1,25 +1,11 @@
 import React from 'react'
-import Input from './Form/Input'
-import Select from './Form/Select';
-import Radio from './Form/Radio';
-import Checkbox from './Form/Checkbox';
+import Produto from './Components/Produto'
+
 const App = () => {
-  const [nome,setNome] = React.useState('');
-  const [email,setEmail] = React.useState('');
-  const [produto, setProduto] = React.useState('')
-  const [cor, setCor] = React.useState('vermelho');
-  const [linguagens, setLinguagens] = React.useState(['PHP']);
-  return (
-    <form>
-      <h2>Checkbox</h2>
-      <Checkbox options={['Javascript','PHP','JAVA']} value={linguagens} setValue={setLinguagens}/>
-      <Radio options={['azul','vermelho']} value={cor} setValue = {setCor}/>
-      <Select options={['smartphone','tablet']} value={produto} setValue={setProduto}/>
-      <Input label="Nome" id="nome" value={nome} setValue={setNome} required/>
-      <Input label="Email" id="email" type="email" value={email} setValue={setEmail} />
-      <button>Enviar</button>
-    </form>
-  )
+  
+  return <div>
+    <Produto/>
+  </div>
 }
 
 export default App
