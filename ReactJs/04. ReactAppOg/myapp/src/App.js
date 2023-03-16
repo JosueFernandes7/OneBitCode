98aa5ac6e6701356fb9eb78bfe2f7dd5d097b8fe
Josue Fernandes <josue7mf@hotmail.com>
2023-03-16 10:13:23 -0300

    PropTypes

--- ReactJs/04. ReactAppOg/myapp/src/App.js
+++ ReactJs/04. ReactAppOg/myapp/src/App.js
@@ -1,32 +1,11 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Header from './Header'
-import Home from './Home'
-import Login from './Login'
-import NaoEncontrada from './NaoEncontrada'
-import Produto from './Produto'
-import Sobre from './Sobre'
-import ProdutoDescricao from './ProdutoDescricao';
-import ProdutoAvaliacao from './ProdutoAvaliacao';
-import ProdutoCustomizado from './ProdutoCustomizado';
+import Button from './Button'
+
 function App() {
   return (
-    <BrowserRouter>
-    <Header/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="login" element={<Login/>}/>
-        <Route path="produto/:id/*" element={<Produto/>}>
-          <Route path="" element={<ProdutoDescricao/>}/>
-          <Route path="avaliacao" element={<ProdutoAvaliacao/>}/>
-          <Route path="customizacao" element={<ProdutoCustomizado/>}/>
-        </Route>
-        <Route path="sobre" element={<Sobre/>}/>
-
-        <Route path="*" element={<NaoEncontrada/>}/>
-      </Routes>
-
-    </BrowserRouter>
+      <div>
+        <Button width={200}>Clique Aqui</Button>
+      </div>
     )
 }
 
---
+++ ReactJs/04. ReactAppOg/myapp/src/Button.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {PropTypes} from 'prop-types';
+
+function Button(props) {
+  return (
+    <button 
+    style={{
+      width: `${props.width}px`,
+      height: `${props.width / 3}px`}}>{props.children}</button>
+  )
+}
+
+Button.propTypes = {
+  width: PropTypes.number.isRequired,
+}
+export default Button
