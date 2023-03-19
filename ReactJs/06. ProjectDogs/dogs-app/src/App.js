f10bfdad6205067f5de66046094283653bf507b3
Josue Fernandes <josue7mf@hotmail.com>
2023-03-19 16:35:03 -0300

    user context

--- ReactJs/06. ProjectDogs/dogs-app/src/App.js
+++ ReactJs/06. ProjectDogs/dogs-app/src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import './App.css'
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Home from './Components/Home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './Components/Login/Login';
+import React from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Components/Login/Login";
+import { UserStorage } from "./UserContext";
 
 function App() {
-  return (
-    <div>
-        <BrowserRouter>
-            <Header/>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/login/*" element={<Login/>}/>
-
-            </Routes>
-            <Footer/>
-        </BrowserRouter>
-    </div>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                <UserStorage>
+                    <Header />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login/*" element={<Login />} />
+                    </Routes>
+                    <Footer />
+                </UserStorage>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
