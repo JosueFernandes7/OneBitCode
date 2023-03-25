c640cf99166faf522f0d60410380856b9741151e
Josue Fernandes <josue7mf@hotmail.com>
2023-03-25 15:28:37 -0300

    criando feed

--- ReactJs/06. ProjectDogs/dogs-app/src/api.js
+++ ReactJs/06. ProjectDogs/dogs-app/src/api.js
@@ -1,68 +1,80 @@
-export const API_URL = 'https://dogsapi.origamid.dev/json';
+export const API_URL = "https://dogsapi.origamid.dev/json";
 
 // verifica  se o token pode ser gerado
 export function TOKEN_POST(body) {
-  return {
-    url: API_URL + '/jwt-auth/v1/token',
-    options: {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    },
-  };
+    return {
+        url: API_URL + "/jwt-auth/v1/token",
+        options: {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        },
+    };
 }
 //  valida o token já existente
 export function TOKEN_VALIDATE_POST(token) {
-  return {
-    url: API_URL + '/jwt-auth/v1/token/validate',
-    options: {
-      method: 'POST',
-      headers: {
-        Authorization: 'Bearer ' + token,
-      },
-    },
-  };
+    return {
+        url: API_URL + "/jwt-auth/v1/token/validate",
+        options: {
+            method: "POST",
+            headers: {
+                Authorization: "Bearer " + token,
+            },
+        },
+    };
 }
 // Verificar usuário
 export function USER_GET(token) {
-  return {
-    url: API_URL + '/api/user',
-    options: {
-      method: 'GET',
-      headers: {
-        Authorization: 'Bearer ' + token,
-      },
-    },
-  };
+    return {
+        url: API_URL + "/api/user",
+        options: {
+            method: "GET",
+            headers: {
+                Authorization: "Bearer " + token,
+            },
+        },
+    };
 }
 // Criar usuário
 // Passar usuario, email, senha
 export function USER_POST(body) {
-  return {
-    url: API_URL + '/api/user',
-    options: {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
+    return {
+        url: API_URL + "/api/user",
+        options: {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
         },
-        body: JSON.stringify(body),
-      },
-  };
+    };
 }
 
 // posta a foto
 
 export function PHOTO_POST(formData, token) {
     return {
-      url: API_URL + '/api/photo',
-      options: {
-        method: 'POST',
-        headers: {
-          Authorization: 'Bearer ' + token,
+        url: API_URL + "/api/photo",
+        options: {
+            method: "POST",
+            headers: {
+                Authorization: "Bearer " + token,
+            },
+            body: formData,
         },
-        body : formData,
-      },
     };
-  }
+}
+
+//   pega as fotos
+
+export function PHOTOS_GET({ page, total, user }) {
+    return {
+        url: `${API_URL}/api/photo/?_page=${page}/&_total=${total}&_user=${user}`,
+        options: {
+            method: "GET",
+            cache: 'no-store'
+        },
+    };
+}
