46cd76ba6ded3508c490708d9b1f627cb88d5249
Josue Fernandes <josue7mf@hotmail.com>
2023-04-03 16:21:53 -0300

    perdeu a senha 1

--- ReactJs/06. ProjectDogs/dogs-app/src/Components/Feed/Feed.js
+++ ReactJs/06. ProjectDogs/dogs-app/src/Components/Feed/Feed.js
@@ -1,7 +1,7 @@
 import React from "react";
 import FeedModal from "./FeedModal";
 import FeedPhotos from "./FeedPhotos";
-
+import PropTypes from "prop-types";
 function Feed({ user }) {
     const [modalPhoto, setModalPhoto] = React.useState(null);
     const [pages, setPages] = React.useState([1]);
@@ -56,5 +56,14 @@ function Feed({ user }) {
         </div>
     );
 }
+Feed.defaultProps = {
+    user: 0,
+};
+Feed.propTypes = {
+    user: PropTypes.oneOfType([
+        PropTypes.string.isRequired,
+        PropTypes.number.isRequired,
+    ]),
+};
 
 export default Feed;
