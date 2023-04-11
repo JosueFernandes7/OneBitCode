5fdc2435ce9d8f4e784e94ee1d9ecd3a94cf1038
Josue Fernandes <josue7mf@hotmail.com>
2023-04-11 10:13:24 -0300

    criando servidor http

--- NodeJs/onebitcodeNode/Production/file.js
+++ NodeJs/onebitcodeNode/Production/file.js
@@ -1,6 +1,4 @@
-const fs = require('fs')
-fs.writeFile('teste.txt',"Node js",err => console.log(err))
-// fs.unlink('teste.txt')
-fs.appendFile('teste.txt','Node 2',err => console.log(err))
-// fs.rename('teste.txt','teste2.txt', err => console.log(err))
-console.log(__filename);
+const fs = require('fs');
+
+
+fs.writeFile('teste.txt',"Hello World!",err => console.log(err))
